8af9efe055bc8661644a2a171f495891fbaddd13
PAhlback <145994681+PAhlback@users.noreply.github.com>
2024-03-05 15:26:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ window.addEventListener('keyup', (e) => {
         alert("You found an easter egg!");
     };
 });
+
+let myDiv = document.getElementById("easteregg1");
+myDiv.addEventListener('click', (e) => {
+    document.querySelector('main').style.backgroundImage = "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.fineartamerica.com%2Fimages%2Fartworkimages%2Fmediumlarge%2F1%2Fthe-brawl-motionage-designs.jpg&f=1&nofb=1&ipt=86da209c301d719801172bdaa41be42a2d8892b56f38caa87a9be81dab28bd97&ipo=images')";
+});
